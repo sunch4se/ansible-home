a04d656dce12c0f98633d4603ce4026ccb8991d2
sunch4se <b.huss@gmx.de>
2020-05-07 07:55:08 +0200

    added backup (cronjob + script)

---
+++ provisioning/files/backup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+### wait for nas.fritz.box to be reachable
+until ping -c1 nas.fritz.box >/dev/null 2>&1; do :; done
+
+### mount nas nfs target
+/usr/bin/sudo /bin/mount /backup
+
+### synch /docker/config to nas
+sudo /usr/bin/rsync -avh --delete /docker/config/ /backup/docker/config/ --dry-run
+
+### umount nas nfs target
+/usr/bin/sudo /bin/umount /backup
