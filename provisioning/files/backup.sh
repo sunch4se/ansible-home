6a1008cfde204ba6172ddb47f30bb800d8c4fbac
sunch4se <b.huss@gmx.de>
2020-05-11 12:39:32 +0200

    modified backup.sh

--- provisioning/files/backup.sh
+++ provisioning/files/backup.sh
@@ -12,5 +12,8 @@ until ping -c1 nas.fritz.box >/dev/null 2>&1; do :; done
 ### synch /docker/config to nas
 sudo /usr/bin/rsync -avh --delete /docker/config/ /backup/docker/config/
 
+### synch .env file for traefik docker
+sudo /usr/bin/rsync -avh /docker/docker-compose/traefik/.env /backup/docker/docker-compose/traefik/
+
 ### umount nas nfs target
 /usr/bin/sudo /bin/umount /backup
