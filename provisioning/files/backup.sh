a476356401435f81ffdd8102e261219862f98513
sunch4se <b.huss@gmx.de>
2020-05-23 08:26:50 +0200

    modified backup.sh

--- provisioning/files/backup.sh
+++ provisioning/files/backup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+BACKUPLOG=/tmp/backuplog
+
 ### wake up nas
 /usr/bin/wakeonlan 00:11:32:51:85:CD
 
@@ -10,12 +12,18 @@ until ping -c1 nas.fritz.box >/dev/null 2>&1; do :; done
 /usr/bin/sudo /bin/mount /backup
 
 ### synch /docker/config to nas
-/usr/local/bin/telegram-send "***** BACKUP COMPLETED *****"
-sudo /usr/bin/rsync --stats -ah --delete /docker/config/ /backup/docker/config/ | /usr/local/bin/telegram-send --stdin
-/usr/local/bin/telegram-send "***** BACKUP COMPLETED *****"
+/usr/bin/sudo /usr/bin/rsync --stats -ah --delete /docker/config/ /backup/docker/config/ > /tmp/backuplog
+
+### telegram send
+/usr/bin/sed -i '1s/^/***** BACKUP COMPLETED *****\n/' /tmp/backuplog
+/usr/bin/printf "\n***** BACKUP COMPLETED *****" >> /tmp/backuplog
+/usr/bin/cat $BACKUPLOG | /usr/local/bin/telegram-send --stdin
 
 ### synch .env file for traefik docker
-sudo /usr/bin/rsync -avh /docker/docker-compose/traefik/.env /backup/docker/docker-compose/traefik/
+/usr/bin/sudo /usr/bin/rsync -avh /docker/docker-compose/traefik/.env /backup/docker/docker-compose/traefik/
 
 ### umount nas nfs target
 /usr/bin/sudo /bin/umount /backup
+
+### cleanup
+/usr/bin/rm $BACKUPLOG
